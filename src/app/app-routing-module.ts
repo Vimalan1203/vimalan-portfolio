446bebba076f084b60c50c505d0c7f245fe220c6
vimalan <vimalan.s@oasyscybernetics.com>
2026-04-20 13:28:54 +0530

    updated

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -34,7 +34,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top', useHash: true })],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
